300bb661c21c7ad22dc23983c024d5601911a38e
Ignst <Example@mail.ru>
2020-03-06 19:40:36 +0300

    Edit

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,6 +1,6 @@
 def binary_searcher(number, massive):
     massive.sort()
-    left = massive[0]
+    left = 0
     right = len(massive) - 1 
     while left <= right:
         mid = (left + right)//2
@@ -13,3 +13,5 @@ def binary_searcher(number, massive):
             right = mid - 1
     return None
 
+a = binary_searcher(23, [21,12,12,3,1,3,123,12,3,23,23,23,4])
+print(a)
